8ec3861d9293b728395f9296ff29e141edc765ca
Gy9vin <Gy9vin@gmail.com>
2018-02-12 18:11:41 +0300

    chain

--- params/config.go
+++ params/config.go
@@ -34,7 +34,7 @@ var (
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainId:        big.NewInt(200666),
+		ChainId:        big.NewInt(200665),
 		HomesteadBlock: big.NewInt(0),
 		DAOForkBlock:   nil,
 		DAOForkSupport: false,
