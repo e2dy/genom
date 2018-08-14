605f10ac4d4aff31edd1defbee95be6767649f63
gy9vin <gy9vin@gmail.com>
2018-08-14 16:03:56 +0200

    swarm/network, swarm/storage: validate chunk size (#17397)
    * swarm/network, swarm/storage: validate default chunk size
    
    * swarm/bmt, swarm/network, swarm/storage: update BMT hash initialisation
    
    * swarm/bmt: move segmentCount to tests
    
    * swarm/chunk: change chunk.DefaultSize to be untyped const
    
    * swarm/storage: add size validator
    
    * swarm/storage: add chunk size validation to localstore
    
    * swarm/storage: move validation from localstore to validator
    
    * swarm/storage: global chunk rules in MRU

--- swarm/network/stream/delivery.go
+++ swarm/network/stream/delivery.go
@@ -24,6 +24,7 @@ import (
 	"github.com/genom-project/genom/common"
 	"github.com/genom-project/genom/metrics"
 	"github.com/genom-project/genom/p2p/discover"
+	cp "github.com/genom-project/genom/swarm/chunk"
 	"github.com/genom-project/genom/swarm/log"
 	"github.com/genom-project/genom/swarm/network"
 	"github.com/genom-project/genom/swarm/spancontext"
@@ -229,6 +230,11 @@ R:
 	for req := range d.receiveC {
 		processReceivedChunksCount.Inc(1)
 
+		if len(req.SData) > cp.DefaultSize+8 {
+			log.Warn("received chunk is bigger than expected", "len", len(req.SData))
+			continue R
+		}
+
 		// this should be has locally
 		chunk, err := d.db.Get(context.TODO(), req.Addr)
 		if err == nil {
@@ -244,6 +250,7 @@ R:
 			continue R
 		default:
 		}
+
 		chunk.SData = req.SData
 		d.db.Put(context.TODO(), chunk)
 
